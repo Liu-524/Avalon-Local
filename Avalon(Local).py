ff642ce117a449cfa55f6f111726d1637cd0e0dc
Liu-524 <55001602+Liu-524@users.noreply.github.com>
2020-03-18 01:22:57 -0500

    complete player distribution

--- Avalon(Local).py
+++ Avalon(Local).py
@@ -13,12 +13,13 @@ def init():
     c_a.c_pass = 0
     c_a.c_deny = 0
     #8 人 局
-    c_a.character = ["莫德雷德(坏人举手不举)", "梅林(看到坏人除了莫德雷德)",
+    character = ["莫德雷德(坏人举手不举)", "梅林(看到坏人除了莫德雷德)",
                     "奥伯伦(看不到坏人队友)", "派西维尔(看梅林和莫甘娜)", 
                     "村民", "村民", "莫甘娜(与梅林一同被派西维尔看到)", 
                     "刺客(坏人失败是刺杀梅林)"]
-    random.seed(time.time)
-    random.shuffle(c_a.character)
+    random.seed(time.time())
+    random.shuffle(character)
+    c_a.character = character
     return render_template("success_page.html")
 
 @app.route("/<player>")
