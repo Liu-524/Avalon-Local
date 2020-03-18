bae60b9ea4b3de97d9bb52b2049793cf5b95bd1b
Liu-524 <55001602+Liu-524@users.noreply.github.com>
2020-03-18 01:07:01 -0500

    add player distribution

--- Avalon(Local).py
+++ Avalon(Local).py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
-from flask import current_app
-
+from flask import current_app as c_a
+import random
+import time
 app = Flask(__name__)
 @app.route("/")
 def index():
@@ -9,9 +10,20 @@ def index():
 
 @app.route("/init")
 def init():
-    current_app.c_pass = 0
-    current_app.c_deny = 0
+    c_a.c_pass = 0
+    c_a.c_deny = 0
+    #8 人 局
+    c_a.character = ["莫德雷德(坏人举手不举)", "梅林(看到坏人除了莫德雷德)",
+                    "奥伯伦(看不到坏人队友)", "派西维尔(看梅林和莫甘娜)", 
+                    "村民", "村民", "莫甘娜(与梅林一同被派西维尔看到)", 
+                    "刺客(坏人失败是刺杀梅林)"]
+    random.seed(time.time)
+    random.shuffle(c_a.character)
     return render_template("success_page.html")
+
+@app.route("/<player>")
+def result(player):
+    return render_template("player.html", player_char = c_a.character[int(player)])
 
 @app.route("/vote")
 def vote():
@@ -19,23 +31,23 @@ def vote():
 
 @app.route("/agree")
 def agree():
-    current_app.c_pass += 1
+    c_a.c_pass += 1
     return render_template("success_page.html")
 
 @app.route("/deny")
 def deny():
-    current_app.c_deny += 1
+    c_a.c_deny += 1
     return render_template("success_page.html")
 
 @app.route("/clear")
 def clear():
-    current_app.c_pass = 0
-    current_app.c_deny = 0
+    c_a.c_pass = 0
+    c_a.c_deny = 0
     return render_template("success_page.html")
 
 @app.route("/display")
 def display():
-    return render_template("display.html", hao = current_app.c_pass, huai = current_app.c_deny)
+    return render_template("display.html", hao = c_a.c_pass, huai = c_a.c_deny)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5002,debug=True)
